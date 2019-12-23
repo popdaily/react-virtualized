f918d1d91e279c2a99c99e1ace93f6006da0b00e
williesmithchen <williesmithchen@gmail.com>
2019-12-23 19:57:50 +0800

    change to lucky 7.

--- source/Masonry/Masonry.example.js
+++ source/Masonry/Masonry.example.js
@@ -267,7 +267,7 @@ export default class GridExample extends React.PureComponent {
         ref={this._setMasonryRef}
         scrollTop={this._scrollTop}
         width={width}
-        fixed={[13]}
+        fixed={[7]}
       />
     );
   }
